067ceb7e64cfd7e6fd3b1dd1b63cd90e01cb4a7f
Ernani Ávila <ernanicavilaneto@gmail.com>
2023-08-08 15:19:47 -0300

    fix: ready to deploy

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -3,9 +3,8 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const client = new QueryClient({});
 
-function Provider({ children }) {
+function Provider({ children }: { children: React.ReactNode }) {
 	return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
 
 export default Provider;
-							
